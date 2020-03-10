c060593593ee54ff719898ce12ba0b2ff7833e53
Greg Martin <gregm31@live.com>
2020-03-10 13:55:10 +1100

    Added pokemon choice + opening line function

--- pokemon.rb
+++ pokemon.rb
@@ -1,3 +1,19 @@
+require "tty-prompt"
+prompt = TTY::Prompt.new
+
+def pokemon_opening_line(name) 
+    case name
+    when "Pikachu"
+        return "May your lightning strike fear into the heart of all mortals! HA!"
+    when "Bulbasaur"
+        return "May you make like a mighty tree, and leaf!.. your oponents ruined on the battlefield... HA!"
+    when "Charmander"
+        return "May you burn them, BURN THEM ALLLLL HA!"
+    when "Squirtle"
+        return "May your water hydrate death! So that he may carry out his duties on your opponents! HA!"
+    end
+end
+
 
 =begin Creating Pokemon class
 pokemon should have
@@ -30,3 +46,7 @@ charmander = Pokemon.new("Charmander", 5, {"Scratch" => 40, "Ember" => 50, "Fire
 bulbasaur = Pokemon.new("Bulbasaur", 8, {"Vine Whip" => 110, "Razor Leaf" => 100, "Ram" => 60}, "Leaf")
 squirtle = Pokemon.new("Squirtle", 10, {"Bubble" => 110, "Tackle" => 60, "Water Gun" => 140}, "Leaf")
 
+p "Welcome to the Pokémon Stadium, where the fiercest trainers do battle! HA!"
+choices = [pikachu.name, charmander.name, bulbasaur.name, squirtle.name]
+user_pokemon = prompt.select("Choose your pokemon!", choices)
+p "You have chosen #{user_pokemon}! #{pokemon_opening_line(user_pokemon)}"
